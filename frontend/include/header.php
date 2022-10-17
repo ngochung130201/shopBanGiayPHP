a1d0e32406e68af5477d4c1abfd17790303f3c3a
nhphat12a <ngpht53@gmail.com>
2022-10-17 22:33:36 +0700

    header update

---
+++ frontend/include/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/frontend/styles/reset.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="/frontend/styles/header.css"> 
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="header-top">
+        <div class="header-top-button-1">
+            <ul>
+                <li><a href="#">Kênh người bán hàng</a></li>
+                <li><a href="#">Trỏ thàn người bán Shopee</a></li>
+                <li><a href="#">Tải ứng dụng</a></li>
+                <li><a href="#">Kết nối</a></li>
+                <li>
+                    <i class='bx bxl-facebook-circle'></i>
+                    <i class='bx bxl-instagram-alt' ></i>
+                </li>
+            </ul>
+        </div>
+        <div class="header-top-button-2">
+            <ul>
+            <li><a href="#"><i class='bx bx-bell'>Thông báo</i></a></li>
+            <li><a href="#"><i class='bx bx-question-mark' >Hỗ Trợ</i></a></li>
+            <li><a href="#"><i class='bx bx-globe'>Ngôn Ngữ</i></a></li>
+            <li><a href="#">Đăng Ký</a></li>
+            <li><a href="">Đăng Nhập</a></li>
+            </ul>
+        </div>
+        </div>
+        <div class="header-mid">
+            <div class="header-mid-logo"><a href="#"><img src="/frontend/assets/img/shopee.png" alt=""></a></div>
+            <div class="header-mid-search">
+            <div class="header-mid-input"><input type="text" name="" id="" placeholder="Nhập"></div>
+            <div class="header-mid-searchbutton"><button><i class='bx bx-search'></i></button></div>
+            </div>
+            <div class="header-mid-cart"><a href="#"><i class='bx bx-cart'></i></a></div>
+        </div>
+        <div class="header-bottom">
+            <ul>
+                <li><a href="#">Dép nữ</a></li>
+                <li><a href="#">Quần Jean Nam</a></li>
+                <li><a href="#">Áo</a></li>
+                <li><a href="#">Nước Hoa</a></li>
+                <li><a href="#">Áo Sweater</a></li>
+                <li><a href="#">Ví Nữ</a></li>
+                <li><a href="#">Đồng Hồ Thông Minh</a></li>
+            </ul>
+        </div>
+    </header>
+    <section></section>
+    <footer></footer>
+</body>
+</html>
