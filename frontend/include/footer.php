fba4f8e310d3756247d31ddb49532be79fceaa0f
HuynhNgocHung <huynhngochungIT@gmail.com.vn>
2022-10-19 09:11:50 +0700

    addadmin

--- frontend/include/footer.php
+++ frontend/include/footer.php
@@ -1,14 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/reset.css">
-    <link rel="stylesheet" href="../styles/footer.css">
-    <title>Document</title>
-</head>
-<body>
+
+<script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+   <script
+   type="text/javascript"
+   src="https://code.jquery.com/jquery-1.11.0.min.js"
+ ></script>
+ <script
+   type="text/javascript"
+   src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"
+ ></script>
+ <script
+   type="text/javascript"
+   src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"
+ ></script>
+ <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" 
+ integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg=="
+  crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
+<script src="../js/slider.js"></script> 
     <footer>
         <div class="footer__support">
           <h4>Chăm sóc khách hàng</h4>
@@ -112,5 +119,3 @@
           </div>
       </footer>
       
-</body>
-</html>
---
+++ frontend/include/slider.php
@@ -0,0 +1,27 @@
+
+    <div class="slider">
+        <div class="slider-autoplay">
+                <div class="image">
+                    <img src="https://cf.shopee.vn/file/5b5de1582d99cbbe75a8f9a5c4dae1c2_xxhdpi" alt="">
+                </div>
+                <div class="image"><img src="https://cf.shopee.vn/file/79ee5b5bc3c772463fb57e07b0ece0b6_xxhdpi" alt="">
+                </div>
+                <div class="image"><img src="https://cf.shopee.vn/file/ccf8ec028180086745c33e88f5414290_xxhdpi" alt="">
+                </div>
+                <div class="image">  
+                    <img src="https://cf.shopee.vn/file/a447a439300ea44a0df8da57d44b7428_xxhdpi" alt="">
+                </div>
+                <div class="image"><img src="https://cf.shopee.vn/file/8924509ad4a7933df914cdb621a04976_xxhdpi" alt="">
+                </div>
+                <div class="image"><img src="ttps://cf.shopee.vn/file/30a33a335c57c1450253950faee958b3_xxhdpi" alt="">
+                </div>
+        </div>
+        <div class="slider-banner">
+            <div class="slider-banner-img-1">
+                <img src="https://cf.shopee.vn/file/14c7d6cbcc0768b4fdf5baa102851145_xhdpi" alt="Shoppee">
+            </div>
+            <div class="slider-banner-img-2">
+                <img src="https://cf.shopee.vn/file/8cbe5b4f1696d077a0c868bb0498d54e_xhdpi" alt="">
+            </div>
+        </div>
+    </div>
